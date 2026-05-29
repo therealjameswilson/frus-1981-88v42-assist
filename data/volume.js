cafde871129e4774f50cb2d7081fb12e1b466565
therealjameswilson <30484292+therealjameswilson@users.noreply.github.com>
2026-05-29 06:59:02 -0400

    Create FRUS assist page

---
+++ data/volume.js
@@ -0,0 +1,12 @@
+window.FRUS_VOLUME = {
+  "index": 7,
+  "doc_id": "frus1981-88v42",
+  "title": "1981\u20131988, Volume XLII, Refugees and Immigration, 1975\u20131984",
+  "official_url": "https://history.state.gov/historicaldocuments/frus1981-88v42",
+  "repo_slug": "frus-1981-88v42-assist",
+  "title_ascii": "1981-1988, Volume XLII, Refugees and Immigration, 1975-1984",
+  "pages_url": "https://therealjameswilson.github.io/frus-1981-88v42-assist/",
+  "repo_url": "https://github.com/therealjameswilson/frus-1981-88v42-assist",
+  "nara_scout_url": "https://therealjameswilson.github.io/nara-scout/#q=XLII+OR+Refugees+OR+Immigration&from=1981&to=1988&sort=relevance&perColl=25&perPage=50",
+  "companion": null
+};
